2d07a5bd94d91ab5447d26cec8d8feac87728762
musulurukranthi_mac <musuluru.kranthi@gmail.com>
2023-05-21 21:29:17 +0530

    Removing gg.java class

--- src/main/java/com/uiauto/drivers/gg.java
+++
@@ -1,9 +0,0 @@
-package com.uiauto.drivers;
-
-public class gg {
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.dir")+"/src/test/resources/Drivers/chromedriver.exe");
-		System.out.println(System.getProperty("os.name"));
-		System.out.println(System.getProperty("os.name"));
-	}
-}
