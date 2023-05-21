dca1106cede08c70978ed7ae70460fd5fa579878
musulurukranthi_mac <musuluru.kranthi@gmail.com>
2023-05-21 21:25:20 +0530

    Added a syso line in the gg.java class

--- src/main/java/com/uiauto/drivers/gg.java
+++ src/main/java/com/uiauto/drivers/gg.java
@@ -4,5 +4,6 @@ public class gg {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir")+"/src/test/resources/Drivers/chromedriver.exe");
 		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.name"));
 	}
 }
